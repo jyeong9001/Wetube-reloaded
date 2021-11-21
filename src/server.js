3708b6e2aeea323500486456bd47cfa85b03ace5
Jaeyeong Kim <91720949+jyeong9001@users.noreply.github.com>
2021-11-21 20:08:38 +0900

    User Authentication
    create account / form validation / status codes / login added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 import express from "express" ;
 import morgan from "morgan" ;
-import globalRouter from './routers/globalRouter';
+import rootRouter from './routers/rootRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
 
@@ -21,7 +21,7 @@ const handleHome = (req, res) => {
 
 app.set("view engine", "pug");
 app.set("views",process.cwd() + "/src/views");
-app.use("/",globalRouter);
+app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/user",userRouter);
 
